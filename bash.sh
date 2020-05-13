8cb19a8b80140fa932770765e1fda42aa9c4ab2b
suysie <stan.suys@gmail.com>
2020-05-13 16:53:04 +0200

    raadspel in bash maar random geeft altijd een 1

--- bash.sh
+++ bash.sh
@@ -1,2 +1,42 @@
-#bashfile
+#!/bin/bash
+getal=$((1 + RANDOM % 10))
+kansen=1
 
+echo 'probeer het getal tussen 1 en 10 te raden'
+
+while [ $kansen -le 3 ]; do
+
+    echo 'poging nummer '$guesses
+
+    read -p "uw getal om te raden is? [n]: " geraden
+
+
+    if [ $geraden -eq $getal ]; then
+
+        echo "u hebt het geraden het te raden getal was: " $getal
+
+        break;
+
+    else
+        if [ $geraden -lt $getal ]; then 
+
+            echo 'het geraden getal is te laag';
+
+        elif [ $geraden -gt $getal ]; then
+
+            echo 'het geraden getal is te hoog';
+
+        fi
+
+    fi
+
+    kansen=$((kansen+1))
+
+done
+
+if [ $geraden -ne $getal ]; then
+
+    echo 'jammer uw 3 pogingen zijn voorbij moesten we meer pogingen geven zou het te gemakkelijk zijn'
+    
+
+fi
